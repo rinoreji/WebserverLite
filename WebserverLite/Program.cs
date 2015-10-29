c72c3f5de6297bea949db119a47640324d916f47
rinoreji <mailrino@gmail.com>
2015-10-30 00:41:29 +0530

    Code and credits added

---
+++ WebserverLite/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Code copied from https://codehosting.net/blog/BlogEngine/post/Simple-C-Web-Server.aspx
+ * All credits goes to the Author at https://codehosting.net/blog/BlogEngine/author/david.aspx
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace WebserverLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://192.168.2.3:8080/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+
+        static string SendResponse(HttpListenerRequest request)
+        {
+            var querystring = new List<string>();
+            foreach (var item in request.QueryString.AllKeys)
+            {
+                querystring.Add(string.Format("{0}={1}", item, request.QueryString.Get(item)));
+            }
+
+            var result = string.Join(",", querystring);
+            Console.WriteLine("incoming request " + result);
+
+            return string.Format("{0} : {1}", DateTime.Now, result);
+        }
+    }
+}
